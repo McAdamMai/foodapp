1362b9378fdd51382cccf7b6e16498f095aef4cc
McAdamMai <maiz7@mcmaster.ca>
2025-09-03 23:54:32 -0400

    add snapshot_writter service

---
+++ snapshot_writer/src/main/java/com/foodapp/snapshot_writer/SnapshotWriterApplication.java
@@ -0,0 +1,13 @@
+package com.foodapp.snapshot_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapshotWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapshotWriterApplication.class, args);
+	}
+
+}
